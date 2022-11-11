4bc448388899937fa9dceb74d233fc47d6790d22
healthy-vitamins <cheeaudrian97@gmail.com>
2022-11-11 17:07:14 +0800

    windows + added more CLI data

--- src/main/java/com/service/banking/FinalBankingApplication.java
+++ src/main/java/com/service/banking/FinalBankingApplication.java
@@ -1,5 +1,8 @@
 package com.service.banking;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -53,13 +56,36 @@ public class FinalBankingApplication {
 			userService.addRoleToUser("admin-user", "ROLE_ADMIN");
 			userService.addRoleToUser("admin-user", "ROLE_USER");
 			
-			Customer cust1 = new Customer(0l, "user", "lastName", "singapore", "12345678", "user@gmail.com", null);
-			BankAccount bankAcc1 = new BankAccount(0l, "Current", 10000f, DateFormatterUtil.currentDateInString(), cust1);
-			bankAccService.saveBankAccAndUser(bankAcc1);
+			List<String> firstNames = Arrays.asList("user", "credit1", "credit2", "credit3", "homeLoan1", "homeLoan2", "carLoan1", "carLoan2");
+			List<String> countries = Arrays.asList("singapore", "malaysia");
+			List<String> numbers = Arrays.asList("12345678", "87654321");
 			
-			Customer cust2 = new Customer(0l, "user2", "lastName2", "malaysia", "87654321", "user2@gmail.com", null);
-			BankAccount bankAcc2 = new BankAccount(0l, "Savings", 5000f, DateFormatterUtil.formatCustomDate("2011-01-01 09:09:09:09"), cust2);
-			bankAccService.saveBankAccAndUser(bankAcc2);
+			List<String> accTypes = Arrays.asList("Overseas", "Savings", "Savings", "Savings", "Savings", "Current", "Savings", "Current");
+			List<Float> accBals = Arrays.asList(5000f ,10000f, 5000f, 7000f, 7000f, 7000f, 10000f, 15000f);
+			List<String> accCreationDates = Arrays.asList("2022-01-01 09:09:09:09", "2011-01-01 09:09:09:09", "2017-01-01 09:09:09:09", "2020-01-01 09:09:09:09", "2020-01-01 09:09:09:09", "2020-01-01 09:09:09:09", "2020-01-01 09:09:09:09", "2020-01-01 09:09:09:09");
+			
+			
+			for(int i = 0; i < firstNames.size(); i++) {
+				Customer cust = new Customer(0l, firstNames.get(i), "lastName", countries.get(i%2), numbers.get(i%2), firstNames.get(i) + "@gmail.com" , null);
+				BankAccount bankAcc = new BankAccount(0l, accTypes.get(i),accBals.get(i), accCreationDates.get(i), cust);
+				bankAccService.saveBankAccAndUser(bankAcc);
+			}
+			
+//			Customer cust1 = new Customer(0l, "user", "lastName", "singapore", "12345678", "user@gmail.com", null);
+//			BankAccount bankAcc1 = new BankAccount(0l, "Current", 10000f, DateFormatterUtil.currentDateInString(), cust1);
+//			bankAccService.saveBankAccAndUser(bankAcc1);
+//			
+//			Customer cust2 = new Customer(0l, "credit1", "lastName", "malaysia", "87654321", "user2@gmail.com", null);
+//			BankAccount bankAcc2 = new BankAccount(0l, "Savings", 10000f, DateFormatterUtil.formatCustomDate("2011-01-01 09:09:09:09"), cust2);
+//			bankAccService.saveBankAccAndUser(bankAcc2);
+//			
+//			Customer cust3 = new Customer(0l, "credit2", "lastName", "china", "12345678", "user3@gmail.com", null);
+//			BankAccount bankAcc3 = new BankAccount(0l, "Savings", 5000f, DateFormatterUtil.formatCustomDate("2017-01-01 09:09:09:09"), cust3);
+//			bankAccService.saveBankAccAndUser(bankAcc3);
+//			
+//			Customer cust4 = new Customer(0l, "credit3", "lastName", "singapore", "87654321", "user4@gmail.com", null);
+//			BankAccount bankAcc4 = new BankAccount(0l, "Savings", 7000f, DateFormatterUtil.formatCustomDate("2020-01-01 09:09:09:09"), cust4);
+//			bankAccService.saveBankAccAndUser(bankAcc3);
 			
 			// credit card
 //			BankAccount bankAcc1 = new BankAccount(null, "Savings", 10000f, DateFormatterUtil.convertDateStringToMillisString("2011-01-01 09:09:09:09"));
